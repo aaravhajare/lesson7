dc1b3bbd934688b2c8dc15998a0ab1495b1458a0
aaravhajare <hajareaarav66@gmail.com>
2025-02-23 09:59:21 +0530

    Customise your ride

---
+++ Customise your ride.py
@@ -0,0 +1,37 @@
+
+print("select one")
+print("bike")
+print("car")
+
+c1 = input("enter your choice")
+
+if (c1 == "bike") :
+    print("what kind of bike")
+    print("ninja H2R\n")
+    print("Duke 490\n")
+
+    c2 = input("select you bike")
+    if (c2 == "ninja H2R") :
+        print("you have selected ninja H2R")
+    else :
+        print("you ahve selected Duke 490")
+
+elif (c1 == "car") :
+
+    print("what kind of bike")
+    print("lambourgine\n")
+    print("bugati\n")
+
+    c3 = input("select you car")
+    if (c2 == "lamborgine") :
+        print("you have selected lambourgine")
+    else :
+        print("you ahve selected bugati")
+
+else :
+    print("you have selected wrong option")
+
+
+
+
+
